461ecc28cc28e71607d7915546850cbed689f729
Dmitry Ryumkin <riumkin@gmail.com>
2017-06-19 20:19:58 +0300

    Small fixes for book details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript' },
-    { title: 'Light of Yoga' },
-    { title: 'Prometey rising' },
-    { title: 'Ruby on Rails' }
+    { title: 'Javascript', pages: 101 },
+    { title: 'Light of Yoga', pages: 704 },
+    { title: 'Prometey rising', pages: 30 },
+    { title: 'Ruby on Rails', pages: 330 }
   ]
 }
